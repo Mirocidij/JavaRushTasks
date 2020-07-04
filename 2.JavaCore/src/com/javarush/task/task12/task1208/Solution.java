44e7c7c2f6f594d066de857bed9b901259f95ea8
Yatsutko Sergey <s3rzh@inbox.ru>
2020-07-04 23:12:23 +0700

    feat: JavaSyntax task 12 complete

---
+++ 2.JavaCore/src/com/javarush/task/task12/task1208/Solution.java
@@ -0,0 +1,17 @@
+package com.javarush.task.task12.task1208;
+
+/* 
+Свобода печати
+*/
+
+public class Solution {
+    public static void main(String[] args) {
+
+    }
+
+    public static void print(String s) { System.out.println(s);    }
+    public static void print(int s) { System.out.println(s);    }
+    public static void print(Integer s) { System.out.println(s);    }
+    public static void print(Character s) { System.out.println(s);    }
+    public static void print(char s) { System.out.println(s);    }
+}
---
+++ 2.JavaCore/src/com/javarush/task/task12/task1210/Solution.java
@@ -0,0 +1,23 @@
+package com.javarush.task.task12.task1210;
+
+/* 
+Три метода и максимум
+*/
+
+public class Solution {
+    public static void main(String[] args) {
+
+    }
+
+    public static int max(int left, int right) {
+        return left > right ? left : right;
+    }
+
+    public static long max(long left, long right) {
+        return left > right ? left : right;
+    }
+
+    public static double max(double left, double right) {
+        return left > right ? left : right;
+    }
+}
