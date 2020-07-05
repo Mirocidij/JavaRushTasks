f742a961f3bb23a162791cd7b3d0404f1ce3d17a
Yatsutko Sergey <s3rzh@inbox.ru>
2020-07-05 13:52:27 +0700

    JavaCore: solve task1910

--- 2.JavaCore/src/com/javarush/task/task19/task1910/Solution.java
+++ 2.JavaCore/src/com/javarush/task/task19/task1910/Solution.java
@@ -4,7 +4,20 @@ package com.javarush.task.task19.task1910;
 Пунктуация
 */
 
+import java.io.*;
+
 public class Solution {
     public static void main(String[] args) {
+        try (BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
+             BufferedReader fileIn = new BufferedReader(new FileReader(in.readLine()));
+             BufferedWriter fileOut = new BufferedWriter(new FileWriter(in.readLine()))) {
+
+            String tmp;
+            while (fileIn.ready()) {
+                tmp = fileIn.readLine().replaceAll("\\p{Punct}", "");
+                fileOut.write(tmp);
+            }
+
+        } catch (IOException ignored) { }
     }
 }
